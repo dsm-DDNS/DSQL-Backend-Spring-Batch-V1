79f20f17334f31b33ba5b66861174767de319c9d
jinu <jinwoo794533@gmail.com>
2022-05-08 17:26:51 +0900

    initial

---
+++ src/test/kotlin/com/ddns/dsqlbatchserver/DsqlBatchServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ddns.dsqlbatchserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DsqlBatchServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
